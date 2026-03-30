7bb66ab31d46be8b907771c0f1e4038330fdf18a
Stinc715 <shennitianchou@outlook.com>
2026-03-30 15:30:01 +0100

    refactor(chat): remove legacy faq matcher implementation

--- backend/src/main/java/com/clubportal/service/ClubChatKbSemanticScorer.java
+++
@@ -1,7 +0,0 @@
-package com.clubportal.service;
-
-import com.clubportal.model.ClubChatKbEntry;
-
-public interface ClubChatKbSemanticScorer {
-    int score(String normalizedMessage, ClubChatKbEntry entry, ChatLanguage language);
-}
